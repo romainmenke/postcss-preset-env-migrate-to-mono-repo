aba66a939ca7c6ca16455cafa4a54a41062dd6a6
romainmenke <romainmenke@gmail.com>
2021-11-01 14:59:20 +0100

    wip

--- scripts/package-json-updates.sh
+++ scripts/package-json-updates.sh
@@ -2,11 +2,16 @@ set -e
 
 cd $NEW_REPO_LOCATION
 
+volta pin node@16
+volta pin npm
+
 npm pkg set name="@csstools/postcss-preset-env-mono-repo"
+npm install --save-dev @stryker-mutator/core
 
 npm pkg set engines.node=">=12" --workspaces
 npm pkg set license="CC0-1.0" --workspaces
 npm pkg set scripts.prepublishOnly="npm run build --if-present && npm run test" --workspaces
+npm pkg set scripts.stryker="stryker run --logLevel error" --workspaces
 
 npm pkg set private=true
 npm pkg set private=true --workspace="@csstools/base-cli"
@@ -14,4 +19,6 @@ npm pkg set private=true --workspace="@csstools/postcss-base-plugin-cli"
 npm pkg set private=true --workspace="@csstools/postcss-base-plugin"
 
 npm pkg delete scripts.prepublishOnly --workspace="@csstools/base-cli"
+npm pkg delete scripts.stryker --workspace="@csstools/base-cli"
+npm pkg delete scripts.stryker --workspace="@csstools/postcss-base-plugin-cli"
 
