798d6cd647dacde36c4b95026baa6c7040b8e5d8
romainmenke <romainmenke@gmail.com>
2022-06-01 18:04:19 +0200

    better

--- scripts/apply-patches.sh
+++ scripts/apply-patches.sh
@@ -5,5 +5,5 @@ startdir="$(pwd)"
 cd $NEW_REPO_LOCATION
 
 for f in "$startdir/scripts/patches/$1"/*; do
-  git am --no-gpg-sign --directory="$2/$1" $f
+  git am -3 --no-gpg-sign --directory="$2/$1" $f
 done
