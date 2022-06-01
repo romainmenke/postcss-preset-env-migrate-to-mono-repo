ec4c00256b7af4c61f8ded1174da3a565bdf6ff3
romainmenke <romainmenke@gmail.com>
2022-06-01 16:51:22 +0200

    rewrite patches

--- scripts/rewrite-patches-csstools-2022-06-01.sh
+++ scripts/rewrite-patches-csstools-2022-06-01.sh
@@ -1,6 +1,6 @@
 set -e
 
-sh ./scripts/rewrite-patches.sh postcss-selector-not plugins
-sh ./scripts/rewrite-patches.sh postcss-attribute-case-insensitive plugins
-sh ./scripts/rewrite-patches.sh postcss-custom-selectors plugins
-sh ./scripts/rewrite-patches.sh postcss-custom-media plugins
+sh ./scripts/rewrite-patches.sh postcss-selector-not csstools
+sh ./scripts/rewrite-patches.sh postcss-attribute-case-insensitive csstools
+sh ./scripts/rewrite-patches.sh postcss-custom-selectors csstools
+sh ./scripts/rewrite-patches.sh postcss-custom-media csstools
